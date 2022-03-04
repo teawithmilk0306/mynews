3529e8c3af4fc7614bd8b40b751b7dc5c6d2d670
yumi <tea.with.mila@outlook.jp>
2022-03-04 13:34:56 +0000

    Laravel 09_2

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,10 @@ Route::get('/', function () {
 
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('profile/edit', 'Admin\ProfileController@edit');
 });
 
-Route::get('profile/create', 'Admin\ProfileController@add');
+//Route::get('admin/profile/create', 'Admin\ProfileController@add');
 
-Route::get('profile/edit', 'Admin\ProfileController@edit');
+//Route::get('admin/profile/edit', 'Admin\ProfileController@edit');
